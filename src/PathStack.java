2a609ac8e4be3dec907a0102baa87ba87a81aef4
wjchan926 <wesley.j.chan@gmail.com>
2017-10-19 15:47:20 -0400

    First Commit

---
+++ src/PathStack.java
@@ -0,0 +1,83 @@
+/**
+ * Generic Stack class that will be used to hold the possible pathways in the
+ * graph. Uses a linked list implementation.
+ * 
+ * @author wchan
+ *
+ */
+public class PathStack<T> {
+
+	private Node top;
+	private int size;
+
+	/**
+	 * Generic node class for PathStack.
+	 * 
+	 * @author wchan
+	 *
+	 */
+	private class Node {
+		private T data;
+		private Node next;
+	}
+
+	/**
+	 * Default constructor for PathStack class.
+	 */
+	PathStack() {
+		top = null;
+		size = 0;
+	}
+
+	/**
+	 * Adds a vertex to the top of the stack. Increases the size of the stack by 1.
+	 * 
+	 * @param t
+	 *            data that will be associated with the node added
+	 */
+	public void push(T t) {
+		Node tempNode = top;
+		top = new Node();
+		top.data = t;
+		top.next = tempNode;
+		size++;
+	}
+
+	/**
+	 * Removes the top vertex from the stack and returns the vertex value. Decreases
+	 * the size of the stack by 1.
+	 * 
+	 * @return value of the vertex at the top of the stack, null if the stack is
+	 *         empty
+	 */
+	public T pop() {
+		if (isEmpty()) {
+			return null;
+		} else {
+			T t = top.data;
+			top = top.next;
+			size--;
+			return t;
+		}
+
+	}
+
+	/**
+	 * Returns but does not remove the top vertex of the stack
+	 * 
+	 * @return value of the vertex at top of the stack, null if the stack is empty
+	 */
+	public T peek() {
+		return isEmpty() ? null : top.data;
+	}
+
+	/**
+	 * Checks if the stack is empty
+	 * 
+	 * @return true if the stack is empty, false otherwise
+	 */
+	private boolean isEmpty() {
+		return top == null;
+	}
+
+}
